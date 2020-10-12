a3c049db29df1105f5a910bd43c0298850d78f2a
Daniel Korsah <dkorsah15@aol.co.uk>
2020-10-12 22:12:29 +0100

    Code to switch between life story stages and use them to edit stats.

---
+++ Assets/Scripts/CharacterCreation/CreationStageSelector.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class CreationStageSelector : MonoBehaviour
+{
+
+    public static bool[] completedStages = { true, false, false, false };
+    //0 - sprite select, 1 - childhood, 2 - adulthood, 3 - later adulthood,
+    public static int progress;
+
+    [SerializeField] private List<GameObject> stagePanels = new List<GameObject>();
+    [SerializeField] private Button backButton;
+    [SerializeField] private TMP_Text nextText;
+
+    void Start()
+    {
+        progress = 0;
+        Resolve();
+    }
+
+    public void Forward()
+    {
+        if (progress > 0 && progress < 4)
+            StatPreview.Next();
+
+        if (completedStages[progress])
+            progress++;
+        Resolve();
+    }
+
+    public void Back()
+    {
+
+        if (progress > 0)
+        {
+            if (progress > 1 && progress < 5)
+                StatPreview.Back(progress);
+
+            //set current stage to inclomplete
+            completedStages[progress] = false;
+
+            //decrement to previous stage
+            progress--;
+
+            //set previous (now current) stage to incomplte
+            if (progress > 0)
+                completedStages[progress] = false;
+        }
+
+
+        Resolve();
+    }
+
+
+    private void Resolve()
+    {
+
+
+        //Don't show back button on first stage
+        //if (progress == 0)
+        //backButton.gameObject.SetActive(false);
+        //else
+        //backButton.gameObject.SetActive(true);
+
+        //if last page show finish instead of Next
+        if (progress == 3)
+            nextText.text = "Finish";
+        else
+            nextText.text = "Next";
+
+        //0,1,2,3 set panels visible or not
+        if (progress < stagePanels.Count)
+        {
+
+            for (int i = 0; i < stagePanels.Count; i++)
+            {
+                if (i == progress)
+                    stagePanels[i].GetComponent<PopDown>().SetDown();
+                else
+                    stagePanels[i].GetComponent<PopDown>().SetUp();
+            }
+        }
+
+        //if progresing past 3rd stage of character creation, set character stats and load next scene
+        if (progress > 3)
+        {
+            CharacterInfo.Stats = new List<int>() { StatPreview.BodyModifierTotal + 1, StatPreview.MindModifierTotal + 1, StatPreview.CharmModifierTotal + 1 }.ToArray();
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/CharacterCreation/StatPreview.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Events;
+
+public class StatPreview : MonoBehaviour
+{
+
+
+    //0 body, 1 mind, 2 charm
+    public static int modifiedStat = -1;
+    public static List<int> modifications = new List<int>();
+
+    public static UnityEvent RefreshStatBlocks = new UnityEvent();
+
+    public static int BodyModifierTotal = 0;
+    public static int MindModifierTotal = 0;
+    public static int CharmModifierTotal = 0;
+
+    [SerializeField] TMP_Text[] statText;
+
+    void Start()
+    {
+        RefreshStatBlocks.AddListener(Refresh);
+
+        statText[0].text = "Body: " + CharacterInfo.Stats[0];
+        statText[1].text = "Mind: " + CharacterInfo.Stats[1];
+        statText[2].text = "Charm: " + CharacterInfo.Stats[2];
+    }
+
+    public void SetModifiedStat(int stat)
+    {
+        modifiedStat = stat;
+        RefreshStatBlocks.Invoke();
+    }
+
+    public static void Next()
+    {
+        if (modifiedStat != -1)
+        {
+            modifications.Add(modifiedStat);
+            CreationStageSelector.completedStages[CreationStageSelector.progress] = true;
+        }
+        modifiedStat = -1;
+    }
+    public static void Back(int stage)
+    {
+        modifiedStat = -1;
+        modifications.RemoveAt(modifications.Count - 1);
+
+    }
+
+    private void Refresh()
+    {
+        //reset from 0 before summing
+        BodyModifierTotal = 0;
+        MindModifierTotal = 0;
+        CharmModifierTotal = 0;
+
+        //sum of all previous modifications
+        foreach (int i in modifications)
+        {
+            if (i == 0)
+                BodyModifierTotal++;
+            else if (i == 1)
+                MindModifierTotal++;
+            else if (i == 2)
+                CharmModifierTotal++;
+        }
+
+        //show changes
+        switch (modifiedStat)
+        {
+            //visual indicators
+            case 0:
+                statText[0].text = "Body: " + (CharacterInfo.Stats[0] + BodyModifierTotal + 1);
+                statText[1].text = "Mind: " + (CharacterInfo.Stats[1] + MindModifierTotal);
+                statText[2].text = "Charm: " + (CharacterInfo.Stats[2] + CharmModifierTotal);
+                break;
+            case 1:
+                statText[0].text = "Body: " + (CharacterInfo.Stats[0] + BodyModifierTotal);
+                statText[1].text = "Mind: " + (CharacterInfo.Stats[1] + MindModifierTotal + 1);
+                statText[2].text = "Charm: " + (CharacterInfo.Stats[2] + CharmModifierTotal);
+                break;
+            case 2:
+                statText[0].text = "Body: " + (CharacterInfo.Stats[0] + BodyModifierTotal);
+                statText[1].text = "Mind: " + (CharacterInfo.Stats[1] + MindModifierTotal);
+                statText[2].text = "Charm: " + (CharacterInfo.Stats[2] + CharmModifierTotal + 1);
+                break;
+            default:
+                Debug.Log("Wrong index");
+                break;
+        }
+    }
+
+}
