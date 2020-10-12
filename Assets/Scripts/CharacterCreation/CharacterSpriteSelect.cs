09cd93a7e7a4feab3580747804707b4441fa22f5
Daniel Korsah <dkorsah15@aol.co.uk>
2020-10-12 15:41:20 +0100

    Add sprite scrolling with buttons.

---
+++ Assets/Scripts/CharacterCreation/CharacterSpriteSelect.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class CharacterSpriteSelect : MonoBehaviour
+{
+    public static UnityEvent CharacterSelectForward = new UnityEvent();
+    public static UnityEvent CharacterSelectBackwards = new UnityEvent();
+
+    [SerializeField] private List<Sprite> characterSprites = new List<Sprite>();
+    private int index;
+    private Image imageComponent;
+
+
+    void Start()
+    {
+        //add listeners so that later controller support can be added
+        CharacterSelectForward.AddListener(ScrollForward);
+        CharacterSelectBackwards.AddListener(ScrollBackwards);
+
+        imageComponent = gameObject.GetComponent<Image>();
+
+        if (characterSprites.Count > 0)
+        {
+            index = 0;
+            imageComponent.sprite = characterSprites[index];
+        }
+
+    }
+
+
+
+    public void ScrollForward()
+    {
+        index++;
+        if (index > characterSprites.Count - 1)
+            index = 0;
+
+        imageComponent.sprite = characterSprites[index];
+    }
+
+    public void ScrollBackwards()
+    {
+        index--;
+        if (index < 0)
+            index = characterSprites.Count - 1;
+
+        imageComponent.sprite = characterSprites[index];
+    }
+}
