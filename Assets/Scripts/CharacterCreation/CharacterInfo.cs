909bc57a40534bd6c8ed9dba8b5fbc980714b1f4
Daniel Korsah <dkorsah15@aol.co.uk>
2020-10-12 22:07:58 +0100

    Add data storage static class for character stats

---
+++ Assets/Scripts/CharacterCreation/CharacterInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CharacterInfo
+{
+
+    public static Sprite PlayerSprite;
+    public static int[] Stats = { 1, 1, 1 };
+
+}
+
+public enum Stat
+{
+    Body,
+    Mind,
+    Charm
+}
