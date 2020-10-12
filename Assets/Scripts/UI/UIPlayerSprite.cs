20852530b48ebf4a0384f09fc2989e15d43e9549
Daniel Korsah <dkorsah15@aol.co.uk>
2020-10-12 22:09:17 +0100

    Draw sprite to image.

---
+++ Assets/Scripts/UI/UIPlayerSprite.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPlayerSprite : MonoBehaviour
+{
+    Image i;
+
+    void Start()
+    {
+        i = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        i.sprite = CharacterInfo.PlayerSprite;
+    }
+}
