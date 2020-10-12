7f52bed2925f473ae5303887f7173d5d84eb7f3a
Daniel Korsah <dkorsah15@aol.co.uk>
2020-10-12 22:14:54 +0100

    Add code to print statblock to UI.

---
+++ Assets/Scripts/UI/StatblockPrinter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StatblockPrinter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        TMP_Text block = GetComponent<TMP_Text>();
+        block.text = string.Format("Body: {0}\nMind: {1}\nCharm: {2}", CharacterInfo.Stats[0], CharacterInfo.Stats[1], CharacterInfo.Stats[2]);
+    }
+
+    // Update is called once per frame
+
+}
