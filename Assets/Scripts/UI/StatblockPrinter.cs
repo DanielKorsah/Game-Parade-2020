5021f7bedc91ae283524b908a86328363a7830e2
Daniel Korsah <dkorsah15@aol.co.uk>
2020-10-13 03:55:14 +0100

    Make UI Stateblock more flexible

--- Assets/Scripts/UI/StatblockPrinter.cs
+++ Assets/Scripts/UI/StatblockPrinter.cs
@@ -2,16 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-
+using UnityEngine.Events;
 public class StatblockPrinter : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static UnityEvent UpdateStatsUI = new UnityEvent();
     void Start()
+    {
+        UpdateStatsUI.AddListener(ForceUpdate);
+        ForceUpdate();
+    }
+
+    public void ForceUpdate()
     {
         TMP_Text block = GetComponent<TMP_Text>();
         block.text = string.Format("Body: {0}\nMind: {1}\nCharm: {2}", CharacterInfo.Stats[0], CharacterInfo.Stats[1], CharacterInfo.Stats[2]);
     }
-
-    // Update is called once per frame
 
 }
