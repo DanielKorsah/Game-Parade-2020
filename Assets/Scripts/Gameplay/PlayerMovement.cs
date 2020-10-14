10a736f7c6525adb1f6b8ab439ca535b90007293
Daniel Korsah <dkorsah15@aol.co.uk>
2020-10-14 03:48:25 +0100

    Basic movement and Dualshock controller support.

---
+++ Assets/Scripts/Gameplay/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float PlayerSpeed;
+
+    private Rigidbody2D body;
+    private Vector2 moveDirection;
+
+    void Start()
+    {
+        body = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        moveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+    }
+    void FixedUpdate()
+    {
+        body.MovePosition((Vector2)gameObject.transform.position + moveDirection * PlayerSpeed * Time.deltaTime);
+    }
+}
