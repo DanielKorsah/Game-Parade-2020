d46684be85d17424b017db49371f538702aa73ea
Daniel Korsah <dkorsah15@aol.co.uk>
2020-10-14 07:09:48 +0100

    Rename idle animation string to avoid errors.

--- Assets/Scripts/Utility/PlayerAnimation.cs
+++ Assets/Scripts/Utility/PlayerAnimation.cs
@@ -17,10 +17,10 @@ public class PlayerAnimation : MonoBehaviour
 {
     private Animator animator;
     private List<List<string>> animSets = new List<List<string>>();
-    private List<string> animStates0 = new List<string>() { "WalkDown0", "WalkUp0", "WalkLeft0", "WalkRight0", "Idle0" };
-    private List<string> animStates1 = new List<string>() { "WalkDown1", "WalkUp1", "WalkLeft1", "WalkRight1", "Idle1" };
-    private List<string> animStates2 = new List<string>() { "WalkDown2", "WalkUp2", "WalkLeft2", "WalkRight2", "Idle2" };
-    private List<string> animStates3 = new List<string>() { "WalkDown3", "WalkUp3", "WalkLeft3", "WalkRight3", "Idle3" };
+    private List<string> animStates0 = new List<string>() { "WalkDown0", "WalkUp0", "WalkLeft0", "WalkRight0", "PlayerIdle0" };
+    private List<string> animStates1 = new List<string>() { "WalkDown1", "WalkUp1", "WalkLeft1", "WalkRight1", "PlayerIdle1" };
+    private List<string> animStates2 = new List<string>() { "WalkDown2", "WalkUp2", "WalkLeft2", "WalkRight2", "PlayerIdle2" };
+    private List<string> animStates3 = new List<string>() { "WalkDown3", "WalkUp3", "WalkLeft3", "WalkRight3", "PlayerIdle3" };
     private string currentAnimaton;
 
 
