1fab30833ced40b3ec80f35b9efb66486deb1072
Daniel Korsah <dkorsah15@aol.co.uk>
2020-10-14 07:08:11 +0100

    Layer sorting by Y axis added.

---
+++ Assets/Scripts/Utility/PositionBasedSortingLayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionBasedSortingLayer : MonoBehaviour
+{
+    [SerializeField] private float offset = 0;
+    [SerializeField] int sortingOrderBase = 5000;
+    private Renderer renderer;
+
+
+
+    void Awake()
+    {
+        renderer = gameObject.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        renderer.sortingOrder = (int)(sortingOrderBase - (transform.position.y - offset));
+    }
+}
