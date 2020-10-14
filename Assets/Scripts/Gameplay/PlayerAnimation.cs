4de9dd1c98f86bc2132c9ad089dcbfb59645c0d0
Daniel Korsah <dkorsah15@aol.co.uk>
2020-10-14 06:26:06 +0100

    Player animates with movement

---
+++ Assets/Scripts/Gameplay/PlayerAnimation.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public enum Directions
+{
+    Down,
+    Up,
+    Left,
+    Right,
+    None
+};
+
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator animator;
+    private List<List<string>> animSets = new List<List<string>>();
+    private List<string> animStates0 = new List<string>() { "WalkDown0", "WalkUp0", "WalkLeft0", "WalkRight0", "Idle0" };
+    private List<string> animStates1 = new List<string>() { "WalkDown1", "WalkUp1", "WalkLeft1", "WalkRight1", "Idle1" };
+    private List<string> animStates2 = new List<string>() { "WalkDown2", "WalkUp2", "WalkLeft2", "WalkRight2", "Idle2" };
+    private List<string> animStates3 = new List<string>() { "WalkDown3", "WalkUp3", "WalkLeft3", "WalkRight3", "Idle3" };
+    private string currentAnimaton;
+
+
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+
+        animSets.Add(animStates0);
+        animSets.Add(animStates1);
+        animSets.Add(animStates2);
+        animSets.Add(animStates3);
+    }
+
+
+    public void ChangeAnimationState(Directions dir)
+    {
+        string newAnimation = animSets[CharacterInfo.PlayerSpriteIndex][(int)dir];
+        if (currentAnimaton == newAnimation)
+        {
+            return;
+        }
+        else
+        {
+            animator.Play(newAnimation);
+            currentAnimaton = newAnimation;
+        }
+    }
+}
--- Assets/Scripts/Gameplay/PlayerMovement.cs
+++ Assets/Scripts/Gameplay/PlayerMovement.cs
@@ -4,19 +4,31 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float PlayerSpeed;
-
+    [SerializeField] private float PlayerSpeed;
+    private PlayerAnimation anim;
     private Rigidbody2D body;
     private Vector2 moveDirection;
 
     void Start()
     {
         body = gameObject.GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<PlayerAnimation>();
     }
 
     void Update()
     {
         moveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+
+        if (moveDirection.x > 0)
+            anim.ChangeAnimationState(Directions.Right);
+        else if (moveDirection.x < 0)
+            anim.ChangeAnimationState(Directions.Left);
+        else if (moveDirection.x == 0 && moveDirection.y > 0)
+            anim.ChangeAnimationState(Directions.Up);
+        else if (moveDirection.x == 0 && moveDirection.y < 0)
+            anim.ChangeAnimationState(Directions.Down);
+        else
+            anim.ChangeAnimationState(Directions.None);
     }
     void FixedUpdate()
     {
